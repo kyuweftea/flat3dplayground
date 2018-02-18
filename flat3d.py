9985eaa7a444aab480879e4d1bfaeaafa3a550a2
Philip Duncan <poduncan@uwaterloo.ca>
2018-02-17 22:43:09 -0500

    scene transforms and affine transforms

--- flat3d.py
+++ flat3d.py
@@ -1,3 +1,4 @@
+import numpy as np
 import gizeh as gz
 import cairocffi as cr
 import moviepy.editor as mpy
@@ -49,29 +50,88 @@ class SurfaceAliased(gz.Surface, object):
 		cxt.set_antialias(cr.ANTIALIAS_NONE)
 		return cxt
 
+def XFscale2d(x, y=None):
+	if (y is None):
+		y = x
+	return [[x, 0, 0],
+	        [0, y, 0],
+	        [0, 0, 1]]
+
+def XFtranslate2d(x, y):
+	return [[1, 0, x],
+	        [0, 1, y],
+	        [0, 0, 1]]
+
+def XFrotate2d(a):
+	return [[ np.cos(a), -np.sin(a), 0],
+	        [ np.sin(a),  np.cos(a), 0],
+	        [         0,          0, 1]]
+
+def rad(a):
+	return np.deg2rad(a)
+
+def XFscale3d(x, y=None, z=None):
+	if (y is None):
+		y = x
+		if (z is None):
+			z = x
+	elif (z is None):
+		z = 1
+	return [[x, 0, 0, 0],
+	        [0, y, 0, 0],
+	        [0, 0, z, 0],
+	        [0, 0, 0, 1]]
+
+def XFtranslate3d(x, y, z):
+	return [[1, 0, 0, x],
+	        [0, 1, 0, y],
+	        [0, 0, 1, z],
+	        [0, 0, 0, 1]]
+
+def XFrotateX3d(a):
+	return [[         1,          0,          0, 0],
+	        [         0,  np.cos(a), -np.sin(a), 0],
+	        [         0,  np.sin(a),  np.cos(a), 0],
+	        [         0,          0,          0, 1]]
+
+def XFrotateY3d(a):
+	return [[ np.cos(a),          0,  np.sin(a), 0],
+	        [         0,          1,          0, 0],
+	        [-np.sin(a),          0,  np.cos(a), 0],
+	        [         0,          0,          0, 1]]
+
+def XFrotateZ3d(a):
+	return [[ np.cos(a), -np.sin(a),          0, 0],
+	        [ np.sin(a),  np.cos(a),          0, 0],
+	        [         0,          0,          0, 0],
+	        [         0,          0,          0, 1]]
+
 class Scene2d(object):
-	def __init__(self, w, h, scale=1):
+	def __init__(self, w, h, scale=1, transform=np.identity(3)):
 		self.w = scale*w
 		self.h = scale*h
 		self.elements = []
 		self.scale = scale
+		self.transform = transform
 	def addElem(self, elem):
 		if (not (elem.isPolygon() or elem.isPolyline() or elem.isDot())):
 			raise TypeError("cannot add element to scene")
 		self.elements.append(elem)
 	def get_gizeh_surface(self):
 		surface = SurfaceAliased(scale=self.scale, width=int(self.w), height=int(self.h))
+		tf = np.matmul(XFscale2d(self.scale), self.transform)
 		for elem in self.elements:
 			if (elem.isPolygon()):
-				points = list(map(lambda x: (self.scale*x[0],self.scale*x[1]), elem.points))
+				points = list(map(lambda x: np.matmul(tf, [x[0], x[1], 1])[:2], elem.points))
 				polygon = gz.polyline(points, fill=elem.fill, close_path=True)
 				polygon.draw(surface)
 			elif (elem.isPolyline()):
-				points = list(map(lambda x: (self.scale*x[0],self.scale*x[1]), elem.points))
+				points = list(map(lambda x: np.matmul(tf, [x[0], x[1], 1])[:2], elem.points))
 				polyline = gz.polyline(points, stroke=elem.stroke, stroke_width=self.scale*elem.width, close_path=elem.closed, line_cap=('butt' if elem.capbutt else 'round'))
 				polyline.draw(surface)
 			elif (elem.isDot()):
-				dot = gz.circle(r=self.scale*elem.width/2.0, xy=(self.scale*elem.point[0],self.scale*elem.point[1]), fill=elem.stroke)
+				x = elem.point
+				dot = gz.circle(r=self.scale*elem.width/2.0, xy=np.matmul(tf, [x[0], x[1], 1])[:2], fill=elem.stroke)
 				dot.draw(surface)
 			else:
 				raise TypeError("cannot make gizeh element")
@@ -100,3 +160,8 @@ def export_vid(name, make_surface, duration, fps=24):
 	mask = mpy.VideoClip(make_mask, duration=duration, ismask=True).resize(1.0/scale)
 	clip = mpy.VideoClip(make_frame, duration=duration).set_mask(mask).resize(1.0/scale)
 	clip.write_videofile("%s.mov" % name, fps=fps, codec='png', with_mask=True)
+
+def export_img(name, surface):
+	filename = "%s.png" % name
+	surface.write_to_png(filename)
+	print "[flat3D]", "Image ready:", filename
--- flat3dtest.py
+++ flat3dtest.py
@@ -7,15 +7,15 @@ import ease
 W,H = 1920,1080 # width, height, in pixels
 duration = 1 # duration of the clip, in seconds
 
-scale = 1.0/4
-# scale = 4
+# scale = 1.0/4
+scale = 4
 # scale = 1
 # scale = 8
 
 def make_surface(t):
 	# print t
 	myease = ease.fancyBezierEase
-	scene = f3d.Scene2d(w=W, h=H, scale=scale)
+	scene = f3d.Scene2d(w=W, h=H, scale=scale, transform=f3d.XFrotate2d(f3d.rad(30*t)))
 	scene.addElem(f3d.Polyline2d([(0,0), (500,600), (500, 800)], stroke=(0,0,1), width=5))
 	scene.addElem(f3d.Polygon2d([(50+50*myease(t),50+50*myease(t)), (70+50*myease(t),60+50*myease(t)), (30+50*myease(t), 100+50*myease(t))], fill=(1,0,0)))
 	scene.addElem(f3d.Polygon2d([(100+200*myease(t),100+200*myease(t)), (70+200*myease(t),60+200*myease(t)), (30+200*myease(t), 100+200*myease(t))], fill=(1,0,0)))
@@ -23,3 +23,4 @@ def make_surface(t):
 	return scene.get_gizeh_surface()
 
 f3d.export_vid('coolMyEffects', make_surface, duration)
+# f3d.export_img('coolMyEffects', make_surface(0))
