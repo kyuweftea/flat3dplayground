3d rotation perspective with ordering bug

flat3d.py
@@ -517,7 +517,7 @@ class Scene3d(Scene):
 		           [v_x[1], v_y[1], v_z[1], 0],
 		           [v_x[2], v_y[2], v_z[2], 0],
 		           [     0,      0,      0, 1]]
-		v = xf.M(xf.translate3d(-self.camera.position[0], -self.camera.position[1], -self.camera.position[2]), np.linalg.inv(v_r_inv))
+		v = xf.M(np.linalg.inv(v_r_inv), xf.translate3d(-self.camera.position[0], -self.camera.position[1], -self.camera.position[2]))
 		scene2d = Scene2d(w=self.w2d, h=self.h2d, scale=self.scale2d, transform=xf.M(xf.translate2d(self.w2d/2.0, self.h2d/2.0), xf.scale2d(self.w2d/2.0, -self.h2d/2.0)))
 		
 		# make tree

flat3dtest3d.py
@@ -1,5 +1,6 @@
 import flat3d as f3d
 import xform as xf
+import numpy as np
 
 W = 1920.0
 H = 1080.0
@@ -10,13 +11,13 @@ scale = 1.0
 # scale = 4.0
 
 def makescene(t):
-	camera = f3d.Camera3d(position=(0,0,0), direction=(0,1,0), up=(0,0,1), fov=xf.rad(90), aspect=W/H, near=1e-6)
+	camera = f3d.Camera3d(position=(3*np.sin(t*np.pi/2.0),-3*np.cos(t*np.pi/2.0),0), direction=(-3*np.sin(t*np.pi/2.0),3*np.cos(t*np.pi/2.0),0), up=(0,0,1), fov=xf.rad(90), aspect=W/H, near=1e-6)
 	scene = f3d.Scene3d(w2d=W, h2d=H, scale2d=scale, camera=camera)
 
-	scene.addElem(f3d.Line3d(points=[(-0.5,0.25,-0.6), (0.5,2,1)], stroke=(0,1,1), width=5))
-	scene.addElem(f3d.Triangle3d(points=[(-1,1,-0.5), (1*t + 0*(1-t), 1*t + 0.25*(1-t), 0*t + -1*(1-t)), (0,1,1)], fill=(1,1,0)))
-	scene.addElem(f3d.Dot3d(point=(-0.5,2*t,0.5), stroke=(0,1,1), width=5))
-	scene.addElem(f3d.Triangle3d(points=[(-1,2,1), (0,0,-1), (1,1,0.5)], fill=(1,0,0)))
+	# scene.addElem(f3d.Line3d(points=[(-0.5,0.25,-0.6), (0.5,2,1)], stroke=(0,1,1), width=5))
+	# scene.addElem(f3d.Triangle3d(points=[(-1,1,-0.5), (1*t + 0*(1-t), 1*t + 0.25*(1-t), 0*t + -1*(1-t)), (0,1,1)], fill=(1,1,0)))
+	# scene.addElem(f3d.Dot3d(point=(-0.5,2*t,0.5), stroke=(0,1,1), width=5))
+	# scene.addElem(f3d.Triangle3d(points=[(-1,2,1), (0,0,-1), (1,1,0.5)], fill=(1,0,0)))
 
 	# scene.addElem(f3d.Triangle3d(points=[(-1,5*t+0.25,0), (1,5*t+0.25,0), (0,5*t+0.25,1)], fill=(0,1,1)))
 	# scene.addElem(f3d.Triangle3d(points=[(-1,5*t+0.5,-0.5), (1,5*t+0.5,0), (0,5*t+0.5,1)], fill=(1,1,0)))
@@ -24,7 +25,10 @@ def makescene(t):
 	# scene.addElem(f3d.Triangle3d(points=[(-1,5*t+0.25,0), (1,5*t+0.25,0), (0,5*t+0.25,1)], fill=(0,1,1)))
 	# scene.addElem(f3d.Triangle3d(points=[(-1,5*t+0.5,-0.5), (1,5*t+0.5,0), (0,5*t+0.5,1)], fill=(1,1,0)))
 	# scene.addElem(f3d.Triangle3d(points=[(-1,1,0), (1,1,0), (0,1,1)], fill=(1,1,1)))
-
+	
+	scene.addElem(f3d.Triangle3d(points=[(-1,2,1), (0,0,-1), (1,1,0.5)], fill=(1,0,0)))
+	scene.addElem(f3d.Triangle3d(points=[(-1,1,-0.5), (1, 1, 0), (0,1,1)], fill=(1,1,0)))
+	
 	return scene
 
 f3d.export_vid('cool3dEffects', makescene, duration)