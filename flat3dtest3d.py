992c83d52b83a031acce0166ea945ca7b8f0ea97
Philip Duncan <poduncan@uwaterloo.ca>
2018-04-13 21:31:20 -0400

    perspective works

--- flat3dtest3d.py
+++ flat3dtest3d.py
@@ -10,13 +10,13 @@ scale = 1.0
 # scale = 4.0
 
 def makescene(t):
-	camera = f3d.Camera3d(position=(0,0,0), direction=(0,1,0), up=(0,0,1), fov=xf.rad(90), aspect=W/H, near=1e-10)
+	camera = f3d.Camera3d(position=(0,0,0), direction=(0,1,0), up=(0,0,1), fov=xf.rad(90), aspect=W/H, near=1e-6)
 	scene = f3d.Scene3d(w2d=W, h2d=H, scale2d=scale, camera=camera)
 
 	scene.addElem(f3d.Line3d(points=[(-0.5,0.25,-0.6), (0.5,2,1)], stroke=(0,1,1), width=5))
 	scene.addElem(f3d.Triangle3d(points=[(-1,1,-0.5), (1*t + 0*(1-t), 1*t + 0.25*(1-t), 0*t + -1*(1-t)), (0,1,1)], fill=(1,1,0)))
 	scene.addElem(f3d.Dot3d(point=(-0.5,2*t,0.5), stroke=(0,1,1), width=5))
-	scene.addElem(f3d.Triangle3d(points=[(-1,2,1), (0,-0.0001,-1), (1,1,0.5)], fill=(1,0,0)))
+	scene.addElem(f3d.Triangle3d(points=[(-1,2,1), (0,0,-1), (1,1,0.5)], fill=(1,0,0)))
 
 	# scene.addElem(f3d.Triangle3d(points=[(-1,5*t+0.25,0), (1,5*t+0.25,0), (0,5*t+0.25,1)], fill=(0,1,1)))
 	# scene.addElem(f3d.Triangle3d(points=[(-1,5*t+0.5,-0.5), (1,5*t+0.5,0), (0,5*t+0.5,1)], fill=(1,1,0)))
